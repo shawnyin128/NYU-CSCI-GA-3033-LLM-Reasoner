6aee8acce3a02bfccd927c4ed9e1cbead49b621b
shawnyin128 <xy2053@nyu.edu>
2026-02-13 14:25:22 -0500

    [train]: update data_load to support online read

--- student/train.py
+++ student/train.py
@@ -2,18 +2,23 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+from student.Transformer import TransformerLM
+from student.optimize import AdamW, cosine_annealing_scheduler, gradient_clipping, cross_entropy
+
 
 def data_load(dataset: np.ndarray, batch_size: int, context_length: int, device: torch.device):
-    dataset_ts = torch.from_numpy(dataset).to(device)
-    n = dataset_ts.numel()
+    n = len(dataset)
     m = context_length
-    valid_bound = n - m
-    offset = torch.arange(m, device=device) # [m]
-    random_starts = torch.randint(0, valid_bound, (batch_size,), device=device) # [B]
-    indices = random_starts.unsqueeze(-1) + offset.unsqueeze(0) # [B, m]
-    shift_indices = indices.clone() + 1
-    inputs = dataset_ts[indices]
-    targets = dataset_ts[shift_indices]
+
+    random_starts = np.random.randint(0, n - m, size=batch_size)  # [B]
+    offset = np.arange(m) # [m]
+    indices = random_starts[:, None] + offset[None, :] # [B, m]
+
+    inputs_np = dataset[indices]
+    targets_np = dataset[indices + 1]
+
+    inputs = torch.from_numpy(inputs_np).to(device)
+    targets = torch.from_numpy(targets_np).to(device)
     return inputs, targets
 
 
@@ -32,6 +37,66 @@ def load_checkpoint(src: str, model: nn.Module, optimizer: torch.optim.Optimizer
     optimizer.load_state_dict(state_dict["optimizer"])
     iteration = state_dict["iteration"]
     return model, optimizer, iteration
+
+
+def train_pipeline():
+    # argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    # determine device
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    # init model
+    model = TransformerLM(vocab_size=args.vocab_size,
+                          context_length=args.context_length,
+                          num_layers=args.num_layers,
+                          d_model=args.d_model,
+                          num_heads=args.num_heads,
+                          d_ff=args.d_ff,
+                          rope=args.rope,
+                          theta=args.theta,
+                          eps=args.eps,
+                          device=device,
+                          dtype=args.dtype).to(device)
+    model.train()
+
+    # build optimizer
+    optimizer = AdamW(params=model.parameters(),
+                      lr=args.lr,
+                      weight_decay=args.weight_decay)
+
+    # load tokenzied data
+    dataset = np.memmap(args.dataset_path, dtype=np.uint16, mode="r")
+
+    # training
+    epochs = args.epochs
+    iterations = args.iterations
+    batch_size = args.batch_size
+    context_length = args.context_length
+    max_l2_norm = args.max_l2_norm
+    for i in range(epochs):
+        for j in range(iterations):
+            inputs, targets = data_load(dataset, batch_size, context_length, device)
+
+            # forward
+            logits = model(inputs)
+
+            # loss compute
+            loss = cross_entropy(logits, targets)
+
+            # gradient zeroing
+            optimizer.zero_grad()
+
+            # backward
+            loss.backward()
+
+            # step
+            optimizer.step()
+    return model
 
 
 if __name__ == '__main__':
