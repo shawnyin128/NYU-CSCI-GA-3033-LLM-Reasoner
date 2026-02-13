f28cb6d1126298cf6fd63bfeb3a32ddf26da381a
Shawn <shawn@macbookpro.mynetworksettings.com>
2026-02-12 23:24:47 -0500

    [BPE]: finish optimized version BPE train

--- student/byte_pair_encoding.py
+++ student/byte_pair_encoding.py
@@ -1,3 +1,4 @@
+import json
 import multiprocessing
 import os
 from collections import defaultdict
@@ -113,6 +114,40 @@ def update_word_freq(word_freq: dict[tuple[bytes], int], pair: tuple[bytes]):
     return new_word_freq
 
 
+def update_word_and_pair_freq(word_freq: dict[tuple[bytes], int], pair: tuple[bytes], pair_freq: dict[tuple[bytes], int]):
+    merged_symbol = pair[0] + pair[1]
+    new_word_freq = defaultdict(int)
+
+    for word, freq in word_freq.items():
+        if pair not in zip(word, word[1:]):
+            new_word_freq[word] += freq
+            continue
+
+        new_word = []
+        i = 0
+        while i < len(word):
+            if i < len(word) - 1 and (word[i], word[i + 1]) == pair:
+                if i > 0:
+                    pair_freq[(word[i - 1], word[i])] -= freq
+                if i < len(word) - 2:
+                    if (word[i + 1], word[i + 2]) != pair:
+                        pair_freq[(word[i + 1], word[i + 2])] -= freq
+                new_word.append(merged_symbol)
+                if i > 0:
+                    pair_freq[(word[i - 1], merged_symbol)] += freq
+                if i < len(word) - 2:
+                    if (word[i + 1], word[i + 2]) != pair:
+                        pair_freq[(merged_symbol, word[i + 2])] += freq
+                i += 2
+            else:
+                new_word.append(word[i])
+                i += 1
+
+        new_word_freq[tuple(new_word)] += freq
+
+    return new_word_freq
+
+
 def train_bpe_naive(input_path: str, vocab_size: int, special_tokens: list[str]):
     vocab = {}
     for i in range(len(special_tokens)):
@@ -139,28 +174,163 @@ def train_bpe_naive(input_path: str, vocab_size: int, special_tokens: list[str])
 
         # merge pair
         ## naive merge
-        merge = []
+        merges = []
         cur_idx = len(vocab)
         while cur_idx < vocab_size:
             pair = get_pair(word_freq)
-            merge.append(pair)
+            merges.append(pair)
             vocab[cur_idx] = pair[0] + pair[1]
             cur_idx += 1
             word_freq = update_word_freq(word_freq, pair)
-    return vocab, merge
+    return vocab, merges
 
 
+def train_bpe_fast(input_path: str, vocab_size: int, special_tokens: list[str]):
+    # steps:
+    # 1. init vocab: init dict, insert special tokens, insert 256 tokens
+    # 2. open file
+    # 3. split chunk
+    # 4. for each chunk, do multiprocessing to pre-tokenize. merge to get global frequency
+    # 5. create pair frequency dict
+    # 6. do merging
 
+    # 1. init vocab
+    vocab = {}
+    for i in range(len(special_tokens)):
+        vocab[i] = special_tokens[i].encode("utf-8")
+    offset = len(special_tokens)
+    for i in range(256):
+        vocab[offset + i] = bytes([i])
 
+    # 2. open file
+    with open(input_path, "rb") as f:
+        # 3. split chunk
+        num_processes = 4
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
+        # 4. multiprocessing: pretokenize
+        tasks = [(input_path, start, end, special_tokens) for start, end in zip(boundaries[:-1], boundaries[1:])]
+        with multiprocessing.Pool(num_processes) as pool:
+            word_freqs = pool.starmap(bpe_pre_tokenize_worker, tasks)
+        word_freq = defaultdict(int)
+        for local_dict in word_freqs:
+            for old_word, freq in local_dict.items():
+                word_freq[old_word] += freq
+
+        # 5. global pair frequency dict
+        pair_freq = defaultdict(int)
+        pair_words = defaultdict(set)
+
+        for old_word, freq in word_freq.items():
+            for i in range(len(old_word) - 1):
+                pair = (old_word[i], old_word[i + 1])
+                pair_freq[pair] += freq
+                pair_words[pair].add(old_word)
+
+        # 6. do merging
+        merges = []
+        cur_idx = len(vocab)
+        while cur_idx < vocab_size:
+            # 6.1. find current best pair
+            best_pair = None
+            max_freq = -1
+            for pair, freq in pair_freq.items():
+                if freq > max_freq or (freq == max_freq and pair > best_pair):
+                    best_pair = pair
+                    max_freq = freq
+
+            # 6.2. append the best merge
+            merges.append(best_pair)
+            merge_token = best_pair[0] + best_pair[1]
+            vocab[cur_idx] = merge_token
+            cur_idx += 1
+
+            # 6.3. find affected words
+            affected_words = pair_words[best_pair]
+            del pair_words[best_pair]
+            del pair_freq[best_pair]
+
+            # 6.4. update frequency
+            for old_word in affected_words:
+                freq = word_freq[old_word]
+
+                # for all pairs in this word, its pair frequency should be updated, like this old pairing is not valid
+                for i in range(len(old_word) - 1):
+                    old_pair = (old_word[i], old_word[i + 1])
+                    # remove the affected pair frequency
+                    pair_freq[old_pair] -= freq
+                    # remove the affected word pair map
+                    pair_words[old_pair].discard(old_word)
+
+                # merge
+                new_word = [] # contains the new byte pairs
+                i = 0
+                while i < len(old_word):
+                    if i < len(old_word) - 1 and (old_word[i], old_word[i + 1]) == best_pair:
+                        new_word.append(merge_token)
+                        i += 2
+                    else:
+                        new_word.append(old_word[i])
+                        i += 1
+                new_word = tuple(new_word)
+
+                # pop up the current word pair from word_freq like it never existed before.
+                # add new word pair to word_freq
+                word_freq.pop(old_word)
+                word_freq[new_word] += freq
+
+                # update pair_freq and pair_words
+                for i in range(len(new_word) - 1):
+                    pair = (new_word[i], new_word[i + 1])
+                    pair_freq[pair] += freq
+                    pair_words[pair].add(new_word)
+    return vocab, merges
 
 
 def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str], mode: str = "fast"):
     if mode == "naive":
         return train_bpe_naive(input_path, vocab_size, special_tokens)
     else:
-        pass
+        return train_bpe_fast(input_path, vocab_size, special_tokens)
+
+
+def save_bpe(vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], output_path: str, type: str = "val"):
+    serializable_vocab = {
+        str(k): v.decode("latin-1")
+        for k, v in vocab.items()
+    }
+
+    with open(os.path.join(output_path, f"vocab_{type}.json"), "w", encoding="utf-8") as f:
+        json.dump(serializable_vocab, f, ensure_ascii=False)
+
+    serializable_merges = [
+        (a.decode("latin-1"), b.decode("latin-1"))
+        for a, b in merges
+    ]
+
+    with open(os.path.join(output_path, f"merges_{type}.json"), "w", encoding="utf-8") as f:
+        json.dump(serializable_merges, f, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    train_bpe_naive("../data/TinyStoriesV2-GPT4-valid.txt", 32000, ["<|endoftext|>"])
+    import time
+    import tracemalloc
+    import cProfile
+    import pstats
+
+    start_time = time.time()
+
+    vocab, merge = train_bpe("../data/TinyStoriesV2-GPT4-train.txt", 10000, ["<|endoftext|>"], "fast")
+
+    end_time = time.time()
+
+    print("Time:", end_time - start_time)
+
+    save_bpe(vocab, merge, "./checkpoint", "train")
+#
+#     cProfile.run(
+#         'train_bpe_naive("../data/TinyStoriesV2-GPT4-valid.txt", 10000, ["<|endoftext|>"])',
+#         'profile_output'
+#     )
+#     stats = pstats.Stats('profile_output')
+#     stats.sort_stats('cumulative').print_stats(20)
--- tests/adapters.py
+++ tests/adapters.py
@@ -595,4 +595,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    return train_bpe(input_path, vocab_size, special_tokens, "naive")
+    return train_bpe(input_path, vocab_size, special_tokens, "fast")
