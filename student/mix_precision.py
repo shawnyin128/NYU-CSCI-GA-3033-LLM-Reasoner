a8923e785598a84d1bde7d8cb7f6e1ba90936b14
Shawn <shawn@b-10-27-79-197.dynapool.vpn.nyu.edu>
2026-03-06 01:14:43 -0500

    [mix-precision]

--- student/mix_precision.py
+++ student/mix_precision.py
@@ -3,20 +3,20 @@ import torch
 s = torch.tensor(0,dtype=torch.float32)
 for i in range(1000):
     s += torch.tensor(0.01,dtype=torch.float32)
-print(s)
+print(s) # tensor(10.0001)
 
 s = torch.tensor(0,dtype=torch.float16)
 for i in range(1000):
     s += torch.tensor(0.01,dtype=torch.float16)
-print(s)
+print(s) # tensor(9.9531, dtype=torch.float16)
 
 s = torch.tensor(0,dtype=torch.float32)
 for i in range(1000):
     s += torch.tensor(0.01,dtype=torch.float16)
-print(s)
+print(s) # tensor(10.0021)
 
 s = torch.tensor(0,dtype=torch.float32)
 for i in range(1000):
     x = torch.tensor(0.01,dtype=torch.float16)
     s += x.type(torch.float32)
-print(s)
+print(s) # tensor(10.0021)
